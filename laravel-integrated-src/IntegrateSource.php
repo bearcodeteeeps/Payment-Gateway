2602e0a93e313571d58a825d78a2e16776d91479
Lloric Mayuga Garcia <lloricode@users.noreply.github.com>
2017-07-29 13:40:12 +0800

    Update IntegrateSource.php

--- laravel-integrated-src/IntegrateSource.php
+++ laravel-integrated-src/IntegrateSource.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace Lloricode\PaymentGateway;
-use Form;
+
 class IntegrateSource
 {
 	public function requireApi()
         {
-                Form::token();
                 require_once __DIR__.'../../cryptobox.class.php';
         }
 }
